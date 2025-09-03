8e6cde76e99195385ae4af20805d18f8d6d37966
Tapan Kumar Padhi <164610686+tapand3v3lop3r@users.noreply.github.com>
2025-09-03 07:14:44 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("");
+        int number = input.nextInt();
+    }
+}
